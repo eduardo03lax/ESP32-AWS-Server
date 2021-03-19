f038490b7581faf15fffc3d470c7d2ef22d22e22
Eduardo García <31087849+eduardo03lax@users.noreply.github.com>
2021-03-19 07:20:56 -0600

    Add files via upload

---
+++ dummy.js
@@ -0,0 +1,30 @@
+const usuarios = [
+    {
+        id: 0,
+        nombre: 'pepe',
+        clave: 'aaammmmffffC'
+
+    },
+    {
+        id: 20,
+        nombre: 'Juan',
+        clave: 'zpfg'
+    },
+    {
+        id: 2,
+        nombre: 'Pancha',
+        clave: 'lllld'
+    },
+    {
+        id: 1,
+        nombre: 'Eustaclio',
+        clave: 'abc'
+    }
+]
+
+//a3m4f4C
+//zpfg
+//l4d
+//abc
+
+module.exports = usuarios;
---
+++ main.js
@@ -0,0 +1,8 @@
+const app = require('./app');
+
+const port = 8000;
+
+app.listen(port, ()=>
+{
+    console.log("servidor corriendo correctamente en el puerto 8000");
+});
